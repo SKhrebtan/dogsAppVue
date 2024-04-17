88ef1a134f246022c980bff10ba182a6ee205d55
Skhrebtan <shrebtan@gmail.com>
2024-04-17 11:25:01 +0300

    some cahnges

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import { onMounted, watchEffect } from 'vue'
+import { ref, onMounted, watchEffect, onUnmounted } from 'vue'
 import { useAuthStore } from './stores/auth'
 import { setToken } from './stores/api/axios'
+import BurgerSVG from './assets/images/burger.svg?url'
+import CloseSVG from './assets/images/close.svg?url'
+const isDesktop = ref(true)
+const showMobile = ref(false)
+console.log(isDesktop)
 const store = useAuthStore()
+const toggleNav = () => {
+  showMobile.value = !showMobile.value
+}
 onMounted(async () => {
   await store.hydrateFromLocalStorage()
   const data = await store.getToken(store.userToken)
 
   await setToken(data.token)
+  isDesktop.value = window.innerWidth >= 1024
+  window.addEventListener('resize', () => {
+    isDesktop.value = window.innerWidth >= 1024
+  })
 })
 // watchEffect(() => {
 //   console.log(store.userAuth)
@@ -16,29 +28,93 @@ onMounted(async () => {
 const signOutUser = () => {
   store.signOut()
 }
+
+onUnmounted(() => {
+  window.removeEventListener('resize', () => {
+    isDesktop.value = window.innerWidth >= 1024
+  })
+})
 </script>
 
 <template>
-  <header>
-    <nav>
+  <header :class="{ 'mobile-header': !isDesktop }">
+    <nav v-if="isDesktop">
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/mydogs">My Dogs</RouterLink>
       <RouterLink v-if="!store.userToken" to="/login">Login</RouterLink>
       <RouterLink v-if="!store.userToken" to="/register">Register</RouterLink>
     </nav>
-    <div v-if="store.userToken" class="user-div">
+    <div v-if="store.userToken && isDesktop" class="user-div">
       <p>{{ store.userAuth.email }}</p>
       <div class="thumb">
         <img :src="store.userAuth.avatar" alt="avatar" />
       </div>
       <button @click="signOutUser">Logout</button>
     </div>
+    <button v-else type="button" @click="toggleNav" class="burger-btn">
+      <img :src="BurgerSVG" alt="My image" class="burger-svg" />
+    </button>
+    <transition name="fade">
+      <div v-if="showMobile" class="mobile-menu">
+        <nav class="mobile-nav">
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/mydogs">My Dogs</RouterLink>
+          <RouterLink v-if="!store.userToken" to="/login">Login</RouterLink>
+          <RouterLink v-if="!store.userToken" to="/register">Register</RouterLink>
+        </nav>
+        <div v-if="store.userToken" class="mobile-user-div">
+          <p>{{ store.userAuth.email }}</p>
+          <div class="thumb">
+            <img :src="store.userAuth.avatar" alt="avatar" />
+          </div>
+          <button @click="signOutUser">Logout</button>
+        </div>
+        <button type="button" @click="toggleNav" class="close-btn">
+          <img :src="CloseSVG" alt="My image" class="close-svg" />
+        </button>
+      </div>
+    </transition>
   </header>
 
   <RouterView />
 </template>
 
 <style scoped>
+.burger-btn {
+  align-self: flex-end;
+}
+.close-btn {
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  background-color: transparent;
+  border: none;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition:
+    opacity 0.5s ease,
+    transform 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  transform: translateX(100%);
+}
+.mobile-menu {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: aqua;
+}
+.burger-svg {
+  width: 20px;
+  height: 20px;
+}
+
 header {
   display: flex;
   flex-direction: row;
@@ -46,7 +122,9 @@ header {
   padding: 10px;
   background-color: blanchedalmond;
 }
-
+.mobile-header {
+  justify-content: flex-end;
+}
 nav {
   width: 100%;
   font-size: 14px;
