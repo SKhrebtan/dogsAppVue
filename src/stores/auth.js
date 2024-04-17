0f4cad836498694d693f8ee3957fea6ca3864d6d
Skhrebtan <shrebtan@gmail.com>
2024-04-17 19:29:49 +0300

    av fix

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -16,7 +16,7 @@ export const useAuthStore = defineStore('authStore', {
   actions: {
     async getToken(token) {
       this.user = await getProfile(token)
-      this.avatar = this.user.avatar
+      // this.avatar = this.user.avatar
       this.token = this.user.token
       setToken(this.user.token)
       this.persistToLocalStorage()
@@ -42,21 +42,29 @@ export const useAuthStore = defineStore('authStore', {
     persistToLocalStorage() {
       localStorage.setItem('user', JSON.stringify(this.user))
       localStorage.setItem('token', JSON.stringify(this.token))
+      localStorage.setItem('avatar', JSON.stringify(this.avatar))
     },
 
     async updateUserAvatar(file) {
       const data = await updateAvatar(file)
       this.avatar = data.avatar
+      this.user.avatar = data.avatar
+      localStorage.setItem('user', JSON.stringify(this.user))
+      localStorage.setItem('avatar', JSON.stringify(this.avatar))
     },
     hydrateFromLocalStorage() {
       const user = localStorage.getItem('user')
       const token = localStorage.getItem('token')
+      const avatar = localStorage.getItem('avatar')
       if (user) {
         this.user = JSON.parse(user)
       }
       if (token) {
         this.token = JSON.parse(token)
         setToken(this.token)
+      }
+      if (avatar) {
+        this.avatar = JSON.parse(avatar)
       }
     }
   },
